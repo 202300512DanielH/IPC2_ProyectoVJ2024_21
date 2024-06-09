5134e9836092d078fa1c98de7a8715d0d45b9394
Daniel Hernandez <3031294090108@ingenieria.usac.edu.gt>
2024-06-08 23:29:48 -0600

    Carga de Productos
    La carga de productos ya contiene validaciones además de almacenarse completamente
    Se agregó el atributo de nombre restante

--- Proyecto1/controller/cargaMasivaProducto.py
+++ Proyecto1/controller/cargaMasivaProducto.py
@@ -1,6 +1,7 @@
 import xml.etree.ElementTree as ET
 import os
 import sys
+from tkinter import messagebox
 
 # Agregando la carpeta models al path de Python
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -16,19 +17,29 @@ class CargaMasivaProducto:
     # Crear una nueva lista circular doblemente enlazada 
     lista_productos = ListaCircularDoblementeEnlazada()
     
-    def __init__(self, archivo_xml):
+    def __init__(self, archivo_xml= None):
         self.archivo_xml = archivo_xml
 
     def cargar_xml(self):
-        arbol = ET.parse(self.archivo_xml)
-        raiz = arbol.getroot()
-        for elemento_producto in raiz.findall('producto'):
-            id = elemento_producto.get('id')
-            precio = float(elemento_producto.find('precio').text)
-            descripcion = elemento_producto.find('descripcion').text
-            categoria = elemento_producto.find('categoria').text
-            cantidad = int(elemento_producto.find('cantidad').text)
-            imagen = elemento_producto.find('imagen').text
-            producto = Producto(id, precio, descripcion, categoria, cantidad, imagen)
-            self.lista_productos.insertar(producto)
+        try:
+            arbol = ET.parse(self.archivo_xml)
+            raiz = arbol.getroot()
+            for elemento_producto in raiz.findall('producto'):
+                id = elemento_producto.get('id')
+                nombre = elemento_producto.find('nombre').text
+                precio = elemento_producto.find('precio').text
+                descripcion = elemento_producto.find('descripcion').text
+                categoria = elemento_producto.find('categoria').text
+                cantidad = elemento_producto.find('cantidad').text
+                imagen = elemento_producto.find('imagen').text
+                producto = Producto(id,nombre, precio, descripcion, categoria, cantidad, imagen)
+                if producto.validar_precio() and producto.validar_cantidad():
+                    self.lista_productos.insertar(producto)
+                else:
+                    print(f"El producto {producto.id} no pasó la validación.")
+                    messagebox.showerror("Error", "Producto(s) no válidos en el archivo XML. Los productos que sí son válidos se han cargado.")
+
+        except Exception as e:
+            print(f"Error durante la carga del XML o la creación del producto {e}")
+
         return self.lista_productos
--- Proyecto1/models/producto.py
+++ Proyecto1/models/producto.py
@@ -1,12 +1,28 @@
 class Producto:
-    def __init__(self, id, precio, descripcion, categoria, cantidad, imagen):
+    def __init__(self, id, nombre, precio, descripcion, categoria, cantidad, imagen):
         self.id = id
+        self.nombre = nombre
         self.precio = precio
         self.descripcion = descripcion
         self.categoria = categoria
         self.cantidad = cantidad
         self.imagen = imagen
 
+    def validar_precio(self):
+        try:
+            float(self.precio)
+            return True
+        except:
+            return False
+
+    def validar_cantidad(self):
+        try:
+            int(self.cantidad)
+            return True
+        except:
+            return False
+
+
     def __str__(self):
-        return (f"ID: {self.id},\n Precio: {self.precio},\n Descripción: {self.descripcion},"
+        return (f"ID: {self.id},\n Precio: {self.precio},\n Nombre: {self.nombre},\n Descripción: {self.descripcion},"
                 f"\n Categoría: {self.categoria},\n Cantidad: {self.cantidad},\n Imagen: {self.imagen}")
