acefb4e891683a33c422e008558ed22523a8c825
Fátima Cerezo <308240220608@ingenieria.usac.edu.gt>
2024-06-08 20:35:15 -0600

    Reporte de Usuarios

--- Proyecto1/models/listaDoblementeEnlazada.py
+++ Proyecto1/models/listaDoblementeEnlazada.py
@@ -58,54 +58,45 @@ class ListaDoblementeEnlazada:
     # Método para graficar la lista de usuarios con Graphviz
     def graficar(self):
         codigo_dot = ''
-        # Verificar si la carpeta "Reportes" existe, y crearla si no
         carpeta_reportes = 'Reportes'
         if not os.path.exists(carpeta_reportes):
             os.makedirs(carpeta_reportes)
 
-        # Abrir el archivo para escribir
         archivo = open(os.path.join(carpeta_reportes, 'Listas_doblemente_enlazadas.dot'), 'w')
         codigo_dot += '''digraph G {
         rankdir=LR;
         node [shape = record, height = .1]'''
-        contador_nodos = 0
 
-        # PRIMERO CREAMOS LOS NODOS
+        # Crear los nodos
+        contador_nodos = 0
         actual = self.primero
         while actual is not None:
-            codigo_dot += f'node{contador_nodos} [label="{{ID: {actual.usuario.id}\\nNombre: {actual.usuario.nombre}\\nEdad: {actual.usuario.edad}\\nEmail: {actual.usuario.email}\\nTeléfono: {actual.usuario.telefono}|<f1>|<f2>}}"];\n'
+            codigo_dot += 'node'+str(contador_nodos)+' [label = \"{<f1>| ID: '+str(actual.usuario.id)+'\\nNombre: '+str(actual.usuario.nombre)+'\\nEdad: '+str(actual.usuario.edad)+'\\nEmail: '+str(actual.usuario.email)+'\\nTelefono: '+str(actual.usuario.telefono)+'|<f2>}"];\n'
             contador_nodos += 1
             actual = actual.siguiente
-
-        # AHORA CREAMOS LAS RELACIONES DE IZQUIERDA A DERECHA
+            
+        #HACEMOS LAS RELACIONES
         actual = self.primero
         contador_nodos = 0
-        while actual.siguiente is not None:
-            codigo_dot += f'node{contador_nodos}:f2 -> node{contador_nodos+1}:f1;\n'
+        while actual.siguiente != None:
+            #RELACIONES DE IZQUIERDA A DERECHA
+            codigo_dot += 'node'+str(contador_nodos)+':f2 -> node'+str(contador_nodos+1)+':f1;\n'
+            #RELACIONES DE DERECHA A IZQUIERDA
+            codigo_dot += 'node'+str(contador_nodos+1)+':f1 -> node'+str(contador_nodos)+':f2;\n'
             contador_nodos += 1
             actual = actual.siguiente
-
-        # AHORA CREAMOS LAS RELACIONES DE DERECHA A IZQUIERDA
-        actual = self.ultimo
-        contador_nodos -= 1  # Ajustamos el contador para apuntar al nodo actual
-        while actual.anterior is not None:
-            codigo_dot += f'node{contador_nodos}:f1 -> node{contador_nodos-1}:f2;\n'
-            contador_nodos -= 1
-            actual = actual.anterior
 
         codigo_dot += '}'
 
-        # Escribimos el código DOT en el archivo
         archivo.write(codigo_dot)
         archivo.close()
 
-        # Generamos la imagen
+        # Generar la imagen y abrir el reporte
         ruta_dot = 'Reportes/Listas_doblemente_enlazadas.dot'
         ruta_imagen = 'Reportes/Listas_doblemente_enlazadas.png'
         comando = 'dot -Tpng ' + ruta_dot + ' -o ' + ruta_imagen
         os.system(comando)
 
-        # Abrimos la imagen
         ruta_abrir_reporte = os.path.abspath(ruta_imagen)
         os.startfile(ruta_abrir_reporte)
         print('Reporte generado con éxito')
