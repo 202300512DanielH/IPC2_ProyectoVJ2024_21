5533623f28b3849596e9ff1dc1ecfe71427b245a
Fátima Cerezo <308240220608@ingenieria.usac.edu.gt>
2024-06-20 18:44:40 -0600

    Modulo Usuario
    El modulo de usuarios es totalmente funcional

--- Proyecto2/Frontend/django_frontend/main/views.py
+++ Proyecto2/Frontend/django_frontend/main/views.py
@@ -42,19 +42,37 @@ def producto_detalle_view(request, producto_id):
     if request.method == 'POST':
         nombre_producto = producto_encontrado['nombre']
         cantidad = request.POST['cantidad']
-        response = requests.post('http://127.0.0.1:5000/add_cart', data={'nombre_producto': nombre_producto, 'cantidad': cantidad})
-        if response.status_code == 200:
-            # Podrías añadir un mensaje de éxito si lo deseas
-            return render(request, 'producto.html', {'producto': producto_encontrado, 'success': 'Producto agregado al carrito exitosamente.'})
-        else:
-            # Mostrar mensaje de error
-            return render(request, 'producto.html', {'producto': producto_encontrado, 'error': 'No se pudo agregar el producto al carrito'})
+        action = request.POST['action']
+        
+        if action == "add_cart": 
+            response = requests.post('http://127.0.0.1:5000/add_cart', data={'nombre_producto': nombre_producto, 'cantidad': cantidad})
+            if response.status_code == 200:
+                # Podrías añadir un mensaje de éxito si lo deseas
+                return render(request, 'producto.html', {'producto': producto_encontrado, 'success': 'Producto agregado al carrito exitosamente.'})
+            else:
+                # Mostrar mensaje de error
+                return render(request, 'producto.html', {'producto': producto_encontrado, 'error': 'No se pudo agregar el producto al carrito'})
+        elif action == "add_buy": 
+            response = requests.post('http://127.0.0.1:5000/comprar')
+            if response.status_code == 200:
+                # Podrías añadir un mensaje de éxito si lo deseas
+                return render(request, 'producto.html', {'producto': producto_encontrado, 'success': 'Compra realizada exitosamente.'})
+            elif response.status_code == 400:
+                # Mostrar mensaje de error
+                return render(request, 'producto.html', {'producto': producto_encontrado, 'error': 'Carrito vacío, no se pudo realizar la compra'})
+            else:
+                return render(request, 'producto.html', {'producto': producto_encontrado, 'error': 'No se pudo realizar la compra'})
     
     return render(request, 'producto.html', {'producto': producto_encontrado})
 
 #vista para descargar el carrito de compras
 def descarga_carrito(request):
     flask_download_url = 'http://localhost:5000/descarga_carrito'
+    return redirect(flask_download_url)
+
+#vista para descargar las compras realizadas
+def descarga_compras(request):
+    flask_download_url = 'http://localhost:5000/descarga_compras'
     return redirect(flask_download_url)
 
 def carga_productos(request):
